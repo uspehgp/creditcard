0beabef0788fd5d37d1f2761c0efc9e9196620c4
uspehgp <uspehgp@gmail.com>
2021-10-02 15:26:59 +0300

    add validators for card form

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -9,14 +9,18 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 export class CardFormComponent implements OnInit {
   cardForm: any = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    cardNumber: new FormControl(''),
-    expiration: new FormControl(''),
-    securityCode: new FormControl('')
+    cardNumber: new FormControl('', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]),
+    expiration: new FormControl('', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]),
+    securityCode: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(3)])
   });
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log('Form was submitted')
   }
 }
